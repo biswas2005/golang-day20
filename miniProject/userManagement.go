e4b35c617bdacfc7fd05e7f132ea5512fef0f935
biswas2005 <biswas2005a@gmail.com>
2026-01-11 13:10:06 +0530

    User Management

--- miniProject/userManagement.go
+++ miniProject/userManagement.go
@@ -19,26 +19,35 @@ var users = []User{
 }
 var idCounter = 2
 
+// createUser() creates a new User
 func createUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var newUser User
 	err := json.NewDecoder(r.Body).Decode(&newUser)
 	if err != nil {
-		fmt.Println("Error Decoding", err)
+		http.Error(w, `{"Error":"Invalid Body"}`, http.StatusBadRequest)
+		return
+	}
+	//validation is checked
+	if !validation(newUser, w) {
 		return
 	}
 	newUser.ID = idCounter
 	idCounter++
+	//newUser is added to the slice
 	users = append(users, newUser)
+
 	w.WriteHeader(http.StatusAccepted)
 	json.NewEncoder(w).Encode(newUser)
 }
 
+// getUser() reads the users from the Slice
 func getUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	idstr := r.URL.Query().Get("id")
 	if idstr != "" {
+		//convert idstr from string to int
 		id, err := strconv.Atoi(idstr)
 		if err != nil {
 			fmt.Println("Error converting ID:", err)
@@ -54,9 +63,12 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+	//reads the users
 	json.NewEncoder(w).Encode(users)
 }
 
+// updateUser() changes the value
+// it takes id as an identifier
 func updateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -67,16 +79,21 @@ func updateUser(w http.ResponseWriter, r *http.Request) {
 	}
 	id, err := strconv.Atoi(parts[2])
 	if err != nil {
-		fmt.Println("Error ID:", err)
+		http.Error(w, `{"Error":"Invalid ID"}`, http.StatusBadRequest)
 		return
 	}
-
+	//updatedUser has the new data
 	var updatedUser User
 	err1 := json.NewDecoder(r.Body).Decode(&updatedUser)
 	if err1 != nil {
-		fmt.Println("Error decoding:", err)
+		http.Error(w, `{"Error":"Invalid Body"}`, http.StatusBadRequest)
 		return
 	}
+	//checks for the validation
+	if !validation(updatedUser, w) {
+		return
+	}
+	//updates the data
 	for i, user := range users {
 		if user.ID == id {
 			users[i].Name = updatedUser.Name
@@ -88,6 +105,7 @@ func updateUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 }
 
+// deleteUser() deletes if data exist
 func deleteUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -114,6 +132,7 @@ func deleteUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 }
 
+// userHandler() switches the operation
 func userHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -135,8 +154,39 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func UserManagement() {
+// validation() checks for some cases
+func validation(user User, w http.ResponseWriter) bool {
+	w.Header().Set("Content-Type", "application/json")
+	//name cannot be empty
+	if strings.TrimSpace(user.Name) == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"Error": "Name cannot be empty."})
+		return false
+	}
+	//email cannot be empty
+	if strings.TrimSpace(user.Email) == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"Error": "Email cannot be empty."})
+		return false
+	}
+	//suffix has to @gmail.com for email
+	if !strings.HasSuffix(user.Email, "@gmail.com") {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"Error": "Wrong Email format."})
+		return false
+	}
+	//prefix must exist
+	prefix := strings.TrimSuffix(user.Email, "@gmail.com")
+	if prefix == "" {
+		http.Error(w, `{"Error:mail cannot be empty"}`, http.StatusBadRequest)
+		return false
+	}
+	return true
+}
 
+func UserManagement() {
+	//Acts as a signal
+	//control the execution
 	http.HandleFunc("/users/", userHandler)
 	fmt.Println("Server running on Path 8080.")
 	http.ListenAndServe(":8080", nil)
